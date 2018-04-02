b99aa28c3a6df8f95a2506ccacc1fe92f14f52c3
eyal.kopelevich <eyal.kopelevich@EYALKOPEL-LT>
2018-04-02 16:52:18 +0300

    j

--- src/test/java/mavenTest/seleniumTest.java
+++ src/test/java/mavenTest/seleniumTest.java
@@ -1,7 +1,9 @@
 package mavenTest;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -18,7 +20,7 @@ import java.net.URL;
 
 public class seleniumTest {
 	private static final long TIMEOUT = 2000;
-	private final String accessKey = "eyJ4cC51Ijo4OCwieHAucCI6MiwieHAubSI6Ik1BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjE4MzIyNDI0MjksImlzcyI6ImNvbS5leHBlcml0ZXN0In0.aVweCqFrmE6UX84F10fBpWPwah66F8Y04DVOk-fhQGE";
+	private final String accessKey = "eyJ4cC51Ijo4OCwieHAucCI6MiwieHAubSI6Ik1BIiwiYWxnIjoiSFMyNTYifQ.eyJleHAiOjE4MzcwNjY4NTMsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.KSvwS-72DOrM9DTUfaJskQZzdA2zREAqW7lYk1J4lO0";
 	private boolean SECURE = false;
 
 	protected URL url;
@@ -29,16 +31,13 @@ public class seleniumTest {
 	@BeforeMethod
 	public void setUp(@Optional("chrome") String browser) {
 		try {
-			url = new URL("https://sales.experitest.com/wd/hub");
+			url = new URL("https://:"+accessKey+ "@sales.experitest.com/wd/hub");
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 //		dc.setCapability(CapabilityType.PLATFORM, Platform.WIN10);
 		dc.setCapability("testName", browser+" Test");
-		dc.setCapability("username", "eyal.kopelevich");
-		dc.setCapability("password", "Experitest2012");
-		dc.setCapability("projectName", "Default");
 		dc.setCapability("generateReport", true);
 		dc.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
 //		dc.setCapability(CapabilityType.BROWSER_NAME, BrowserType.FIREFOX);
@@ -50,21 +49,25 @@ public class seleniumTest {
 	}
 
 	@Test
-	public void test() {
+	public void test() throws InterruptedException {
 		
-		driver.get("https://sales.experitest.com/");
-		driver.findElement(By.xpath("//*[@name='username']")).sendKeys("eyal.kopelevich");
-	    driver.findElement(By.xpath("//*[@name='password']")).sendKeys("Experitest2012");
-	    driver.findElement(By.xpath("//*[@name='login']")).click();
-//	    driver.findElement(By.xpath("//*[@href='#/launchpad']")).click();
-//	    driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[4]")).click();
-//	    int i = 0;
-//	    while (i < 3) {
-//	        driver.findElement(By.xpath("//*[@class='icon icon-smartphone']")).click();
-//	        driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[4]")).click();
-//	        i++;
-//	    }
+		driver.get("https://www.google.com");
+        Thread.sleep(8000);
+        WebElement searchBar = driver.findElement(By.id("lst-ib"));
+        searchBar.click();
+        Thread.sleep(5000);
+        searchBar.sendKeys("Jerusalem wiki");
+        searchBar.sendKeys(Keys.RETURN);
+//        driver.findElement(By.xpath("//*[@id=\"hplogo\"]")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@href=\"https://en.wikipedia.org/wiki/Jerusalem\"]")).click();
+        Thread.sleep(5000);
 
+        driver.findElement(By.xpath("//*[contains(text(), 'History of Jerusalem')]")).click();
+        Thread.sleep(5000);
+        driver.getCurrentUrl();
+        driver.getTitle();
+		
 		
 		try {
 			Thread.sleep(10000);
